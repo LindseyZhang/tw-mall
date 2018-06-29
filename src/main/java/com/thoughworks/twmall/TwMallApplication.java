ae7c4a6de40bb63ef66c80b7918242800cf9811a
张林苹ST800310 <ST800310@cmbvdi.com>
2018-06-29 21:06:38 +0800

    init

---
+++ src/main/java/com/thoughworks/twmall/TwMallApplication.java
@@ -0,0 +1,12 @@
+package com.thoughworks.twmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwMallApplication.class, args);
+	}
+}
